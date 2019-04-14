96306f4f4f367f97483b759df4ad20d301fd6fb5
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-14 16:09:25 -0400

    Add Chalk to Colour Console For Third Challenge

--- intro-node-course/notes-app/app.js
+++ intro-node-course/notes-app/app.js
@@ -1,6 +1,10 @@
+const chalk = require('chalk')
 const notes = require('./notes')
+
+const format = text => chalk.bold.inverse.green.bgRed(text)
 
 const msg = notes()
 
 console.log(msg)
 
+console.log(format("Success!"))
