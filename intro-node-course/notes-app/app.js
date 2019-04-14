c41d9a9db636d3b4e08ea3618e36bf61dff50ebc
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-14 15:58:37 -0400

    Remove Validator From App Not Needed For Challenge

--- intro-node-course/notes-app/app.js
+++ intro-node-course/notes-app/app.js
@@ -1,11 +1,6 @@
-const validator = require('validator')
 const notes = require('./notes')
 
 const msg = notes()
 
 console.log(msg)
 
-console.log(`is Email: Valid: ${validator.isEmail('jason@email.com')}`)
-console.log(`is Email: Valid: ${validator.isEmail('@email.com')}`)
-console.log(`is URL: Valid: ${validator.isURL('http://jstats.me')}`)
-console.log(`is URL: Valid: ${validator.isURL('http:jstats.me')}`)
