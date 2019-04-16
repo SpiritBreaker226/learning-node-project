a5232dc79b4faa74e3e8c96f96875cf122228b9f
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-15 21:19:34 -0400

    Add Yargs to Handle Commend Line Arguments
    Add Yargs to handle command line arguments as it can parse the string
    and provide functions such as version or help which are commend commands
    thus not having to create our string parser.
    
    Ref:
    
    https://www.npmjs.com/package/yargs

--- intro-node-course/notes-app/app.js
+++ intro-node-course/notes-app/app.js
@@ -1,9 +1,22 @@
-const command = process.argv[2]
+const yargs = require('yargs')
 
-console.log(process.argv)
+// Customise yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-  console.log('Adding note!')
-} else if (command === 'remove') {
-  console.log('Removing note!')
-}
+// add, remove, read, list
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => console.log('Adding a new note!'),
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => console.log('Removing the note'),
+})
+
+
+
+console.log(yargs.argv)
