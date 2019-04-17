86fb4029b9c212aef7261bd337a047dd4db3f297
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-16 21:28:25 -0400

    Add Load Notes Functions to Note Module
    Add Load Notes function to notes module to load in the `notes.json` file
    for any function that would want to use that file

--- intro-node-course/notes-app/notes.js
+++ intro-node-course/notes-app/notes.js
@@ -1,8 +1,23 @@
-const addNote = (title, body) => {
+const fs = require('fs')
 
+const addNote = (title, body) => {
+  const notes = loadNotes()
+
+  console.log(notes)
 }
 
 const notes = () => 'Your notes...'
+
+const loadNotes = () => {
+  try {
+    const dataBuffer =  fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
+}
 
 module.exports = {
   add: addNote,
