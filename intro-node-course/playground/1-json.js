b9793056f69ddc772c6ded308d9c380db7b4910d
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-16 21:01:26 -0400

    Update JSON File With Info For Sixth Challenge
    Update JSON File with a person info for the sixth challenge.

--- intro-node-course/playground/1-json.js
+++ intro-node-course/playground/1-json.js
@@ -1,15 +1,11 @@
 const fs = require('fs')
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Hoilday',
-}
-
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json', bookJSON)
-
 const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
-const data = JSON.parse(dataJSON)
+const user = JSON.parse(dataJSON)
 
-console.log(data.title)
+user.name = 'Jason'
+user.age = 38
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
