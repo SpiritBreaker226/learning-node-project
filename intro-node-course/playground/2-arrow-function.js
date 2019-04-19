eafe40217abd25c39dc811e284645336faa884fb
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-19 14:11:35 -0400

    Add Arrow Function Example of Difference ES5 & ES6
    Add the example for arrow functions to show the difference between both
    ES5 and ES6.

---
+++ intro-node-course/playground/2-arrow-function.js
@@ -0,0 +1,32 @@
+// Function Style
+const square = function (x) {
+  return x * x
+}
+
+// Arrow Functions
+const willReturnMutipleLinesAndTwoOrMoreArg = (x, y) => {
+  const grid = x * y
+
+  return willReturnSingleLineAndSingleArg(grid)
+}
+
+// Arrow Function Short Term Syntax
+
+// Returning a single line and argument
+const willReturnSingleLineAndSingleArg = x => x * x
+
+// Not returning a single line and argument
+const willNotReturnSingleLineAndSingleArg = x => (x * x)
+
+// Not returning a single line and 2 or more arguments
+const willNotReturnSingleLineAndTwoOrMoreArg = (x, y) => (x * y)
+
+// Returning a single line and 2 or more arguments
+const willReturnSingleLineAndTwoOrMoreArg = (x, y) => x * y
+
+console.log(square(2))
+console.log(willReturnSingleLineAndSingleArg(3))
+console.log(willNotReturnSingleLineAndSingleArg(4))
+console.log(willNotReturnSingleLineAndTwoOrMoreArg(5, 6))
+console.log(willReturnSingleLineAndTwoOrMoreArg(7, 8))
+console.log(willReturnMutipleLinesAndTwoOrMoreArg(9, 0))
