5abfe444162ee3957e423b6e16926fd97d257f70
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-21 13:52:50 -0400

    Add Weather Context on Other Dark Sky API Sections
    Add weather context by using other Dark Sky API section Daily to show
    that the developer is not limited to the current section and that the
    `res` is an object that can be used, as such.

--- intro-node-course/weather-app/app.js
+++ intro-node-course/weather-app/app.js
@@ -4,6 +4,7 @@ const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/37
 
 request({ url, json: true }, (error, res) => {
   const { temperature, precipProbability } = res.body.currently
+  const { data } = res.body.daily
 
-  console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
+  console.log(`${data[0].summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
 })
