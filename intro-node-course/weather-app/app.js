380021898422bbdb8c6306d13d1d8690905ac01b
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-21 13:23:39 -0400

    Add Weather Text For Fourteenth Challenge

--- intro-node-course/weather-app/app.js
+++ intro-node-course/weather-app/app.js
@@ -3,5 +3,7 @@ const request = require('request')
 const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/37.8267,-122.4233`
 
 request({ url, json: true }, (error, res) => {
-  console.log(res.body.currently)
+  const { temperature, precipProbability } = res.body.currently
+
+  console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
 })
