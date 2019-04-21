aceabf585c8a12feb2045f7f2948fec52cf3095a
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-21 13:16:03 -0400

    Add Request JSON Option to Auto JSON Parse

--- intro-node-course/weather-app/app.js
+++ intro-node-course/weather-app/app.js
@@ -2,9 +2,6 @@ const request = require('request')
 
 const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/37.8267,-122.4233`
 
-request({ url }, (error, res) => {
-  const data = JSON.parse(res.body)
-
-  console.log(data)
-  console.log(data.currently)
+request({ url, json: true }, (error, res) => {
+  console.log(res.body.currently)
 })
