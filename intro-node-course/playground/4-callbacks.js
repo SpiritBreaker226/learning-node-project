bc157b56aeed6887eba58fc97baa5112b7bbf620
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-22 20:56:22 -0400

    Add Callback Within Another Callback
    Add a Callback within another callback to show that the developer could
    wait and get data from a server then calls another callback that could
    change the data thus having multiple uses.

--- intro-node-course/playground/4-callbacks.js
+++ intro-node-course/playground/4-callbacks.js
@@ -11,3 +11,20 @@ setTimeout(() => {
 const names = ['Jason', 'Jen', 'Jess']
 
 const shortName = names.filter((name) => name.length <= 4)
+
+// Callback within another callback
+
+const geocode = (address, callback) => {
+  setTimeout(() => {
+    const data = {
+      latitude: 0,
+      longitude: 0,
+    }
+
+    callback(data)
+  }, 2000)
+}
+
+geocode('Toronto', (data) => {
+  console.log(data)
+})
