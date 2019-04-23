7fb7edbf26f4f47e3e9e397ec6563cc86b0bc22f
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-22 20:37:23 -0400

    Add Callback Function Example
    Add example for callback functions to show what is a callback function
    which is a function pass as an argument and executed within that
    function later on.

---
+++ intro-node-course/playground/4-callbacks.js
@@ -0,0 +1,13 @@
+// Callbacks Examples
+
+// Use with Node API
+
+setTimeout(() => {
+  console.log('Two seconds are up')
+}, 2000)
+
+// Native JavaScript
+
+const names = ['Jason', 'Jen', 'Jess']
+
+const shortName = names.filter((name) => name.length <= 4)
