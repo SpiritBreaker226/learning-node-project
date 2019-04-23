dfb7a6a1b43e3f4e57c1437d4278fe825eac1227
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-22 22:08:07 -0400

    Add Callback Func Get Localion Then Weather Data
    Add callback functions to get the location from Mapbox for now which
    later on get the weather data from Dark Skys.

--- intro-node-course/weather-app/app.js
+++ intro-node-course/weather-app/app.js
@@ -1,2 +1,28 @@
 const request = require('request')
 
+const geocode = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.MAPBOX_API_KEY}`
+
+  request({ url, json: true }, (error, res) => {
+    if (error) {
+      return callback('Unable to connect to location server!')
+    } else if (res.body.features.length === 0) {
+      return callback('Unable to find location')
+    }
+
+    callback(undefined, {
+      latitude: res.body.features[0].center[1],
+      longitude: res.body.features[0].center[0],
+      location: res.body.features[0].place_name,
+    })
+  })
+}
+
+geocode('Ottawa', (error, data) => {
+  if (error) {
+    console.log(error)
+    return
+  }
+
+  console.log(data)
+})
