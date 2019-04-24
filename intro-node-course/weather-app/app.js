936f02e6b7513e1289e2cd92e544e06d3e8e5c0a
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-23 20:46:45 -0400

    Add Forecast Function For Chain Callback Functions
    Add the function forecast to geocode to chain callbacks together so that
    the geocode gets the location data and sends them to the forecast API
    thus get the weather for that location.
    
    Chain Callback is when one callback calls another callback and gives
    more context to the next callback so that they can make other function
    calls with the data.

--- intro-node-course/weather-app/app.js
+++ intro-node-course/weather-app/app.js
@@ -1,16 +1,12 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Ottawa', (error, data) => {
-  if (error) {
-    console.log(error)
-    return
-  }
-
-  console.log(data)
-})
-
-forecast(-75.7088, 44.1545, (error, data) => {
+geocode('Ottawa', (error, location) => {
   console.log('Error', error)
-  console.log('Data', data)
+  console.log('Data', location)
+
+  forecast(location.latitude, location.longitude, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
 })
