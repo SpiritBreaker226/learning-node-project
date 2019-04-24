8e63be18aa8658d9433eefc00e52caff37096c70
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-23 20:58:24 -0400

    Refactor Weather `app.js` to be More Readable
    Refactor the Weather `app.js` to be more readable by having to display
    an error message when there is an error and clean up message to the user
    on success getting the data from Dark Sky.

--- intro-node-course/weather-app/app.js
+++ intro-node-course/weather-app/app.js
@@ -2,11 +2,16 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 geocode('Ottawa', (error, location) => {
-  console.log('Error', error)
-  console.log('Data', location)
+  if (error) {
+    return console.log(error)
+  }
 
-  forecast(location.latitude, location.longitude, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+  forecast(location.latitude, location.longitude, (error, forecast) => {
+    if (error) {
+      return console.log(error)
+    }
+
+    console.log(location.name)
+    console.log(forecast)
   })
 })
