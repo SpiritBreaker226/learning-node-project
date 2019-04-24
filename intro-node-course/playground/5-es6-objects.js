14123ff8a68694b01d232e5db464fc5dc8f41e48
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-23 21:29:52 -0400

    Add Example For Object Destructuring
    Add an example for object destructuring for complex objects or if an
    object has an element that, is used, all the time.

--- intro-node-course/playground/5-es6-objects.js
+++ intro-node-course/playground/5-es6-objects.js
@@ -10,3 +10,24 @@ const user = {
 }
 
 console.log(user)
+
+// Object destructuring
+
+const product = {
+  label: 'Red notebook',
+  price: 3,
+  stock: 201,
+  salePrice: undefined,
+}
+
+const { label: productName, stock, rating = 5} = product
+
+console.log(productName)
+console.log(stock)
+console.log(rating)
+
+const transaction = (type, { label, stock }) => {
+  console.log(type, label, stock)
+}
+
+transaction('order',  product)
