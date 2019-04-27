e6b2c9ceb1929272c8830a276be6fb91ddb833d7
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-26 21:38:13 -0400

    Add More Routes to Express For 21th Callenge

--- intro-node-course/web-server/src/app.js
+++ intro-node-course/web-server/src/app.js
@@ -11,6 +11,14 @@ app.get('/help', (req, res) => {
   res.send('Help page')
 })
 
+app.get('/about', (req, res) => {
+  res.send('About page')
+})
+
+app.get('/weather', (req, res) => {
+  res.send('Show Wather')
+})
+
 // start server and listen on a port
 app.listen(3000, () => {
   console.log('Server is up on port 3000')
