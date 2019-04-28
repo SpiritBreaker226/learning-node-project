c90e21b3d78482176165fdceee3cf19235a7423f
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-27 21:04:10 -0400

    Add Help & About HTML Files For 23th Challenge

--- intro-node-course/web-server/src/app.js
+++ intro-node-course/web-server/src/app.js
@@ -7,14 +7,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-  res.send('Help Page')
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>')
-})
-
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is currently 50 degrees out',
