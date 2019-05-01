7e64ea4e08d7be2b9197642070cc9fe8e83e2c79
Jason Stathopulos <jstathopulos@gmail.com>
2019-04-30 21:24:47 -0400

    Add Handlebar Partials to Views
    Add some handlebar partials which are snippets of HTML that can be
    loaded into an HTML page and be a template where a single change can
    affect multiple pages are the same time.
    
    Also, Nodemon needs to use `-e js,hbs` in its command line so that so
    that it can auto rebuild when '.hbs' files are changed.

--- intro-node-course/web-server/src/app.js
+++ intro-node-course/web-server/src/app.js
@@ -7,10 +7,13 @@ const app = express()
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
