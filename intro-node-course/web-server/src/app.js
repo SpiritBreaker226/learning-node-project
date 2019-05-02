b1c8792680d62b712d9f08827939badcf4ffcc8b
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-01 21:35:11 -0400

    Remove Products Route No Longer Being Use

--- intro-node-course/web-server/src/app.js
+++ intro-node-course/web-server/src/app.js
@@ -43,7 +43,7 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: 'You mush provide an address'
+      error: 'You must provide an address'
     })
   }
 
@@ -51,20 +51,6 @@ app.get('/weather', (req, res) => {
     forecast: 'It is currently 50 degrees out',
     localion: 'New York',
     address: req.query.address,
-  })
-})
-
-app.get('/products', (req, res) => {
-  if (!req.query.search) {
-    return res.send({
-      error: 'You must provide a search term'
-    })
-  }
-
-  console.log(req.query)
-
-  res.send({
-    proudcts: [],
   })
 })
 
