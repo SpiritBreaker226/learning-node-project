678c98950bd585ac1dd9d5df10e42e072b387a07
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-15 21:29:01 -0400

    Add ObjectID to MongoDB JS
    Add the ObjectID to `mongoDB.js` to show how ObjectIDs work with Node.

--- intro-node-course/task-manager/mongodb.js
+++ intro-node-course/task-manager/mongodb.js
@@ -1,8 +1,15 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+
+console.log(`ObjectID ID ${id}`)
+console.log('ObjectID Timestamp: ', id.getTimestamp())
+console.log('ObjectID Hash ', id.id)
+console.log(`ObjectID Size ${id.id.length}`)
+console.log(`What is Being Stored for ObjectID ${id.toHexString().length}`)
 
 MongoClient.connect(
   connectionURL,
@@ -14,26 +21,26 @@ MongoClient.connect(
 
     const db = client.db(databaseName)
 
-    db.collection('tasks')
-      .insertMany([
-        {
-          description: 'Add insertMany',
-          completed: false,
-        },
-        {
-          description: 'Add Fields',
-          completed: true,
-        },
-        {
-          description: 'Run Script',
-          completed: false,
-        },
-      ], (error, result) => {
-        if (error) {
-          console.log('Unable to insert task')
-        }
+    // db.collection('tasks')
+    //   .insertMany([
+    //     {
+    //       description: 'Add insertMany',
+    //       completed: false,
+    //     },
+    //     {
+    //       description: 'Add Fields',
+    //       completed: true,
+    //     },
+    //     {
+    //       description: 'Run Script',
+    //       completed: false,
+    //     },
+    //   ], (error, result) => {
+    //     if (error) {
+    //       console.log('Unable to insert task')
+    //     }
 
-        console.log(result.ops)
-      })
+    //     console.log(result.ops)
+    //   })
   }
 )
