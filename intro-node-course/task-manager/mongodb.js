997dd4c72cd13ac8a5d25e7e4c11aba1fbb9c957
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-16 20:03:04 -0400

    Remove Example Code From MongoDB JS, No Longer Use

--- intro-node-course/task-manager/mongodb.js
+++ intro-node-course/task-manager/mongodb.js
@@ -3,14 +3,6 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-
-console.log(`ObjectID ID ${id}`)
-console.log('ObjectID Timestamp: ', id.getTimestamp())
-console.log('ObjectID Hash ', id.id)
-console.log(`ObjectID Size ${id.id.length}`)
-console.log(`What is Being Stored for ObjectID ${id.toHexString().length}`)
-
 MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true },
@@ -20,27 +12,5 @@ MongoClient.connect(
     }
 
     const db = client.db(databaseName)
-
-    // db.collection('tasks')
-    //   .insertMany([
-    //     {
-    //       description: 'Add insertMany',
-    //       completed: false,
-    //     },
-    //     {
-    //       description: 'Add Fields',
-    //       completed: true,
-    //     },
-    //     {
-    //       description: 'Run Script',
-    //       completed: false,
-    //     },
-    //   ], (error, result) => {
-    //     if (error) {
-    //       console.log('Unable to insert task')
-    //     }
-
-    //     console.log(result.ops)
-    //   })
   }
 )
