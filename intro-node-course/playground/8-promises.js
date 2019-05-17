784cd5eda58ea5af1079f96a501271eae3886b4b
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-16 20:53:15 -0400

    Add Promises Function Example
    Add example for Promises functions to show what is a Promises function
    which is a function pass as an argument and executed within that
    function later on.

---
+++ intro-node-course/playground/8-promises.js
@@ -0,0 +1,54 @@
+// Since Callbacks are basic Promises show the callback
+// then what the Promise would lo look like
+
+// Callback
+
+const doWorkCallback = (callback) => {
+  setTimeout(() => {
+    console.log('When using callback on error')
+
+    callback('This is my error')
+
+    console.log('When using callback for results')
+
+    callback(undefined, [1, 4, 7])
+  }, 2000)
+}
+
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error)
+  }
+
+  console.log(result)
+})
+
+// Promise
+
+const doWorkPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // if call then 'then' Callback is run
+    // if resulve is call then the function does not go any further
+    // same with reject once call the function returns
+    // resolve([7, 4, 1])
+
+    // if call then catch Callback is run
+    reject('Things went wrong!')
+  }, 2000)
+})
+
+doWorkPromise
+  .then((result) => { console.log('Success!', result) })
+  // catch is the new syntc for Promise so that you do not have two
+  // arguments in one then making them easier to read
+  .catch((error) => { console.log('Error!', error) })
+
+
+// Visually of How Promises Works
+//
+//                                        fulfilled
+//                                       /
+// Promise        -- know as pending -->
+//                                       \
+//                                        rejected
+//
