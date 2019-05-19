fa97d03d21f7c7bb2ff6b993c85c159a483d25e3
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-19 12:00:36 -0400

    Add Example For MongoDB DeleteMany
    Ref:
    
    https://mongodb.github.io/node-mongodb-native/3.2/api/Collection.html#deleteMany

--- intro-node-course/task-manager/mongodb.js
+++ intro-node-course/task-manager/mongodb.js
@@ -13,5 +13,9 @@ MongoClient.connect(
 
     const db = client.db(databaseName)
 
+    db.collection('users')
+      .deleteMany({ age: 28 })
+      .then((result) => console.log(result))
+      .catch((error) => console.log('Unable to delete'))
   }
 )
