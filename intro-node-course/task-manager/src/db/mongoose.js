1b81ac32ca7d7a7b5b6d9931401e03fce1235a55
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-20 18:11:09 -0400

    Add Validation Field to User Model
    Add validation field to User model showing the key/value and function
    validations.
    
    Ref:
    
    https://mongoosejs.com/docs/validation.html
    https://mongoosejs.com/docs/schematypes.html

--- intro-node-course/task-manager/src/db/mongoose.js
+++ intro-node-course/task-manager/src/db/mongoose.js
@@ -13,10 +13,17 @@ const User = mongoose.model(
   {
     name: {
       type: String,
+      required: true,
+      trim: true,
     },
     age: {
       type: Number,
-
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error('Age must be a postive number')
+        }
+      }
     }
   }
 )
