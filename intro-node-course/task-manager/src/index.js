6a136ea22a1e4be5f5b69c706e04342f692db4c1
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-21 21:27:15 -0400

    Add Proper Status Code of 201 on For Created
    Add the proper status code of 201 on the creation of User & Task as that
    is the actual status code when creating an element using API since it
    tells the client that something was created instead of a general status
    code of 200.

--- intro-node-course/task-manager/src/index.js
+++ intro-node-course/task-manager/src/index.js
@@ -15,7 +15,11 @@ app.post('/users', (req, res) => {
 
   user
     .save()
-    .then(() => { res.send(user) })
+    .then(() => {
+      res
+        .status(201)
+        .send(user)
+    })
     .catch((error) => {
       res
         .status(400)
@@ -28,7 +32,11 @@ app.post('/tasks', (req, res) => {
 
   task
     .save()
-    .then(() => { res.send(task) })
+    .then(() => {
+      res
+        .status(201)
+        .send(task)
+    })
     .catch((error) => {
       res
         .status(400)
