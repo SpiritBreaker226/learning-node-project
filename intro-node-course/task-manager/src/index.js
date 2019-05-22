5d6d6fd21d4db8a0e8f6fd8773bf9ad514c65074
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-21 20:56:45 -0400

    Add User Model to Index & Create a User
    Add the User model to Task Manager index then created a User using the
    REST API.

--- intro-node-course/task-manager/src/index.js
+++ intro-node-course/task-manager/src/index.js
@@ -2,13 +2,24 @@ const express = require('express')
 
 require('./db/mongoose')
 
+const User = require('./models/user')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
 app.post('/users', (req, res) => {
-  res.send('testing')
+  const user = new User(req.body)
+
+  user
+    .save()
+    .then(() => { res.send(user) })
+    .catch((error) => {
+      res
+        .status(400)
+        .send(error)
+    })
 })
 
 app.listen(port, () => {
