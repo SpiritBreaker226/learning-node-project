6c0fb6593954e2b0d47401053cf9fc60af1f0e49
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-23 21:10:38 -0400

    Covert User REST API to Async-Await

--- intro-node-course/task-manager/src/index.js
+++ intro-node-course/task-manager/src/index.js
@@ -10,55 +10,52 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
 
-  user
-    .save()
-    .then(() => {
-      res
-        .status(201)
-        .send(user)
-    })
-    .catch((error) => {
-      res
-        .status(400)
-        .send(error)
-    })
+  try {
+    await user.save()
+
+    res
+      .status(201)
+      .send(user)
+  } catch(error) {
+    res
+      .status(400)
+      .send(error)
+  }
 })
 
-app.get('/users', (req, res) => {
-  User
-    .find()
-    .then((users) => {
-      res.send(users)
-    })
-    .catch(() => {
-      res
-        .status(500)
-        .send()
-    })
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find()
+
+    res.send(users)
+  } catch(error) {
+    res
+      .status(500)
+      .send()
+  }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const { id: _id } = req.params
 
-  User
-    .findById(_id)
-    .then((user) => {
-      if (user) {
-        return res.send(user)
-      }
+  try {
+    const user = await User.findById(_id)
 
-      res
-        .status(404)
-        .send()
-    })
-    .catch(() => {
-      res
-        .status(500)
-        .send()
-    })
+    if (user) {
+      return res.send(user)
+    }
+
+    res
+      .status(404)
+      .send()
+  } catch(error) {
+    res
+      .status(500)
+      .send()
+  }
 })
 
 app.post('/tasks', (req, res) => {
