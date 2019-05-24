864d31a952b0189cf32e52de46d30d28b23c982e
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-23 20:40:16 -0400

    Update Task Manager Promise Chain With Async-Await
    Update the Task Manager promise chain for Users with Async-Await to show
    how to convert promise chaining into Async-Await.

--- intro-node-course/task-manager/playground/promise-chaining.js
+++ intro-node-course/task-manager/playground/promise-chaining.js
@@ -2,15 +2,16 @@ require('../src/db/mongoose')
 
 const User = require('../src/models/user')
 
-User
-  .findByIdAndUpdate('5ce33015635279c7ebd1bff6', { age: 1 })
-  .then((user) => {
-    console.log(user)
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age })
 
-    return User.countDocuments({ age: 1 })
-  })
-  .then((result) => {
-    console.log(result)
+  return count
+}
+
+updateAgeAndCount('5ce33015635279c7ebd1bff6', 2)
+  .then((count) => {
+    console.log(count)
   })
   .catch((error) => {
     console.log(error)
