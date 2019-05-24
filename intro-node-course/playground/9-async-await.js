8d3fc7f8a87ee3ab6c2d5230c97d79ced08df45c
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-23 20:31:21 -0400

    Show How Promise Reject Works in Async Await
    Show how a Promise reject works when in an Async-Await which is that
    once a Promise, is rejected, the catch, is automatically called and the
    other Await functions do not fire similar to how to try catch works when
    code fails to catch is then called, and all of the code does not work.

--- intro-node-course/playground/9-async-await.js
+++ intro-node-course/playground/9-async-await.js
@@ -1,15 +1,19 @@
 const add = (a, b) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      if (a < 0 || b < 0 ) {
+        reject('Number must be non-negitive')
+      }
+
       resolve(a + b)
     }, 2000)
   })
 }
 
 const doWork = async () => {
-  const sum = await add(1, 99)
+  const sum = await add(1, -99)
   const sum2 = await add(sum, 50)
-  const sum3 = await add(sum2, 3)
+  const sum3 = await add(sum2, -3)
 
   return sum3
 }
