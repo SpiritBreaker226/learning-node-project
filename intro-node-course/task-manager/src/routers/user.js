0dfbe7f932896cef6d534a0ec732370bbbb08315
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-27 21:26:36 -0400

    Move User & Task Routes to Own Files
    Move the routes for User and Task to their files to have separate routes
    for each section do not have a single file with every section in them
    and making the application hard to maintain.

---
+++ intro-node-course/task-manager/src/routers/user.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const User = require('../models/user')
+
+const router = new express.Router()
+
+router.post('/users', async (req, res) => {
+  const user = new User(req.body)
+
+  try {
+    await user.save()
+
+    res
+      .status(201)
+      .send(user)
+  } catch(error) {
+    res
+      .status(400)
+      .send(error)
+  }
+})
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find()
+
+    res.send(users)
+  } catch(error) {
+    res
+      .status(500)
+      .send()
+  }
+})
+
+router.get('/users/:id', async (req, res) => {
+  const { id: _id } = req.params
+
+  try {
+    const user = await User.findById(_id)
+
+    if (user) {
+      return res.send(user)
+    }
+
+    res
+      .status(404)
+      .send()
+  } catch(error) {
+    res
+      .status(500)
+      .send()
+  }
+})
+
+router.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedFields = ['name', 'email', 'password', 'age']
+  const isValid = updates.every(update => allowedFields.includes(update))
+
+  if (!isValid) {
+    return (
+      res
+        .status(400)
+        .send({ error: 'Invalid updates!' })
+    )
+  }
+
+  try {
+    const user = (
+      await User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true , runValidators: true }
+      )
+    )
+
+    if (user) {
+      return res.send(user)
+    }
+
+    res
+      .status(404)
+      .send()
+  } catch(error) {
+    res
+      .status(400)
+      .send(error)
+  }
+})
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    if (user) {
+      return res.send(user)
+    }
+
+    res
+      .status(404)
+      .send()
+  } catch(error) {
+    res
+      .status(500)
+      .send()
+  }
+})
+
+module.exports = router
