bca6836ff25be2b001ab0609b8e61712170bd581
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-27 21:20:26 -0400

    Add Example of How to Use Express Router
    Add an example of how to use Express router, which will help separate
    the routes to, be organized, in different files without having to be in
    a single file.

--- intro-node-course/task-manager/src/index.js
+++ intro-node-course/task-manager/src/index.js
@@ -10,6 +10,13 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+const router = new express.Router()
+
+router.get('/test', (req, res) => {
+  res.send('This is from my other router')
+})
+app.use(router)
+
 app.post('/users', async (req, res) => {
   const user = new User(req.body)
 
