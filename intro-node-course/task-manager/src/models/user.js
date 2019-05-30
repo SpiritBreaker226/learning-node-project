fda2b56dd89105734ec4f91a7e487f93d3c44018
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-29 21:27:44 -0400

    Add Tokens Array & Token Object to User Model
    Add a tokens array and token object to User model so to track the number
    of tokens the user is using as they can be login to different devices so
    not have to log out of every device when the logout. The developer can
    remove a single token for that device while keeping the user login to
    other devices.

--- intro-node-course/task-manager/src/models/user.js
+++ intro-node-course/task-manager/src/models/user.js
@@ -40,12 +40,21 @@ const userSchema = new mongoose.Schema({
         throw new Error('Age must be a postive number')
       }
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    }
+  }]
 })
 
 userSchema.methods.generateAuthToken = async function() {
   const user = this
   const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SERECT)
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
 
   return token
 }
