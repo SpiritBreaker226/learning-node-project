ce7af1440b6602c175c3fd19e6498a2b6778b0e1
Jason Stathopulos <jstathopulos@gmail.com>
2019-05-30 21:14:38 -0400

    Add Middleware Folder & Auth For Authiorizes
    Add the middleware folder where all Express middle were is located as
    that is the pattern when using them since there could be many of them
    that could use, in not only all the routes but also an original route.
    
    Also, add Auth to do authorizes to make sure that the user can do what
    they are authorized to do.

---
+++ intro-node-course/task-manager/src/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async(req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '')
+    const decoded = jwt.verify(token, process.env.JWT_SERECT)
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    })
+
+    if (user) {
+      req.user = user
+      return next()
+    }
+
+    throw new Error()
+  } catch(error) {
+    res
+      .status(401)
+      .send({ error: 'Please authenticate.' })
+  }
+}
+
+module.exports = auth
