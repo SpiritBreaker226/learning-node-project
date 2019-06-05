3003bef7118e2d8a9b8f330ba4feccabb1f77bfb
Jason Stathopulos <jstathopulos@gmail.com>
2019-06-04 20:31:01 -0400

    Update `toJSON` to be Serialization For User Model
    Update the `toJSON` function to be serialization for the User model
    instead of using the custom function as any User instance uses `toJSON`
    to get the data from the User model. As a result, we can override that
    function to serialization so that any data that the developer needs is
    going to get the serialized form since no one needs the password or
    token outside of the User model.

--- intro-node-course/task-manager/src/routers/user.js
+++ intro-node-course/task-manager/src/routers/user.js
@@ -14,7 +14,7 @@ router.post('/users', async (req, res) => {
 
     res
       .status(201)
-      .send({ user: user.getPublicProfile(), token })
+      .send({ user, token })
   } catch(error) {
     res
       .status(400)
@@ -28,7 +28,7 @@ router.post('/users/login', async (req, res) => {
     const user = await User.findByCredentials(email, password)
     const token = await user.generateAuthToken()
 
-    res.send({ user: user.getPublicProfile(), token })
+    res.send({ user, token })
   } catch (error) {
     res
       .status(400)
