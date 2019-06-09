3bcc5cca282031b063d9c066fa51f96a1506327a
Jason Stathopulos <jstathopulos@gmail.com>
2019-06-09 15:33:24 -0400

    Add Auth to Delete Task For User in 53th Challenge

--- intro-node-course/task-manager/src/routers/task.js
+++ intro-node-course/task-manager/src/routers/task.js
@@ -91,17 +91,22 @@ router.patch('/tasks/:id', auth, async (req, res) => {
   }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id)
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    })
 
-    if (task) {
-      return res.send(task)
+    if (!task) {
+      return (
+        res
+          .status(404)
+          .send()
+      )
     }
 
-    res
-      .status(404)
-      .send()
+    res.send(task)
   } catch (error) {
     res
       .status(500)
