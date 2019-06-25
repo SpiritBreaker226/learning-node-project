f7be94e848fa2e538aa38b48e5e1fac082b49c96
Jason Stathopulos <jstathopulos@gmail.com>
2019-06-24 20:33:36 -0400

    Add Temperature Jest Test For 62th Challenge

--- intro-node-course/task-manager/tests/math.test.js
+++ intro-node-course/task-manager/tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require('../src/math')
+const {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
+} = require('../src/math')
 
 test('should calculate total with tip', () => {
   const total = calculateTip(10, .3)
@@ -11,3 +15,16 @@ test('Should calculate total with default tip', () => {
 
   expect(total).toBe(12.5)
 })
+
+test('Should convert 32 F to 0 C', () => {
+  const temporary = fahrenheitToCelsius(32, 0)
+
+  expect(temporary).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+  const temporary = celsiusToFahrenheit(0, 32)
+
+  expect(temporary).toBe(32)
+})
+
