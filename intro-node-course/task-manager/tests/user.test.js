cf645d35b979cc34ace8641fb69889932038feb9
Jason Stathopulos <jstathopulos@gmail.com>
2019-07-08 20:42:11 -0400

    Add Update User Fields Test For 67th Challenge

--- intro-node-course/task-manager/tests/user.test.js
+++ intro-node-course/task-manager/tests/user.test.js
@@ -122,3 +122,25 @@ test('Should upload avatar image', async () => {
 
   expect(user.avatar).toEqual(expect.any(Buffer))
 })
+
+test('Should update valid user fields', async () => {
+  const newName = faker.name.findName()
+
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({ name: newName })
+    .expect(200)
+
+  const user = await User.findById(userOneId)
+
+  expect(user.name).toBe(newName)
+})
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({ location: faker.address.city() })
+    .expect(400)
+})
