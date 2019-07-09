499c9434eec6e318fd067a164c5d1ecc2d9900b7
Jason Stathopulos <jstathopulos@gmail.com>
2019-07-08 21:36:19 -0400

    Add Task Test Suite & Create Task Test

---
+++ intro-node-course/task-manager/tests/task.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest')
+const faker = require('faker')
+
+const app = require('../src/app')
+const Task = require('../src/models/task')
+
+const {
+  setupDatabase,
+  userOne,
+  userOneId,
+} = require('./fixtures/db')
+
+beforeEach(setupDatabase)
+
+test('Should create task for user', async () => {
+  const taskInfo = { description: faker.random.word() }
+
+  const res = await request(app)
+    .post('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send(taskInfo)
+    .expect(201)
+
+  const task = await Task.findById(res.body._id)
+  expect(task).not.toBeNull()
+  expect(task.completed).toBe(false)
+
+  expect(res.body).toMatchObject({ ...taskInfo, owner: userOneId.toString() })
+})
