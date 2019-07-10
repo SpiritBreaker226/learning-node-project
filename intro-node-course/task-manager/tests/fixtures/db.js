6173240ebbadb2c20b4859b81162db4f77512f74
Jason Stathopulos <jstathopulos@gmail.com>
2019-07-09 20:20:47 -0400

    Add Three Test Tasks to Test Task

--- intro-node-course/task-manager/tests/fixtures/db.js
+++ intro-node-course/task-manager/tests/fixtures/db.js
@@ -2,6 +2,7 @@ const faker = require('faker')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 
+const Task = require('../../src/models/task')
 const User = require('../../src/models/user')
 
 const userOneId = new mongoose.Types.ObjectId()
@@ -26,11 +27,37 @@ const userTwo = {
   }],
 }
 
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: faker.random.word(),
+  complete: false,
+  owner: userOneId,
+}
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: faker.random.word(),
+  complete: false,
+  owner: userOneId,
+}
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: faker.random.word(),
+  complete: false,
+  owner: userTwoId,
+}
+
 const setupDatabase = async () => {
   await User.deleteMany()
+  await Task.deleteMany()
 
   await new User(userOne).save()
   await new User(userTwo).save()
+
+  await new Task(taskOne).save()
+  await new Task(taskTwo).save()
+  await new Task(taskThree).save()
 }
 
 module.exports = {
