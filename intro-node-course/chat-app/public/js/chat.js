13ed979d0a2a8efc4d7080179430e83175b76527
Jason Stathopulos <jstathopulos@gmail.com>
2019-07-22 21:40:01 -0400

    Add Chat Box & Send All Clients in 73rd Challenge
    Add a chat box then send a reply to all clients in the 73rd challenge to
    show that clients send data to the server then the server can send back
    the results to all clients.

--- intro-node-course/chat-app/public/js/chat.js
+++ intro-node-course/chat-app/public/js/chat.js
@@ -3,3 +3,11 @@ const socket = io()
 socket.on('message', (message) => {
   console.log(message)
 })
+
+document.querySelector('#message-form').addEventListener('submit',  (e) => {
+  e.preventDefault()
+
+  const message = document.querySelector('#chat-box').value
+
+  socket.emit('sendMessage', message)
+})
--- intro-node-course/chat-app/src/index.js
+++ intro-node-course/chat-app/src/index.js
@@ -20,6 +20,10 @@ io.on('connection', (socket) => {
   console.log('New WebSocket connection')
 
   socket.emit('message', 'Welcome!')
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message)
+  })
 })
 
 server.listen(port, () => {
