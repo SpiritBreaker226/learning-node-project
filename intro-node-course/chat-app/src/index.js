5d7dd5180316c7d07fb152bb6a35b8ce4bfdb2ce
Jason Stathopulos <jstathopulos@gmail.com>
2019-07-23 20:48:28 -0400

    Add Event For Disconnect Using `socket.on`
    Add an event for when a disconnect is a trigger by using `socket.on`
    since the socket can tell if the user is has disconnected; thus, it can
    use.

--- intro-node-course/chat-app/src/index.js
+++ intro-node-course/chat-app/src/index.js
@@ -25,6 +25,10 @@ io.on('connection', (socket) => {
   socket.on('sendMessage', (message) => {
     io.emit('message', message)
   })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!')
+  })
 })
 
 server.listen(port, () => {
