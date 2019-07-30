3e0966b7b97b91df0503dfdbf9f5e052f69b01d2
Jason Stathopulos <jstathopulos@gmail.com>
2019-07-29 20:19:22 -0400

    Add Generate Message Helper Function For Server
    Add a helper function called Generate Message for the Server side, as
    timestamps are going to be added and to reduce duplicated coding when
    sending text messages to the client.

--- intro-node-course/chat-app/src/index.js
+++ intro-node-course/chat-app/src/index.js
@@ -5,6 +5,8 @@ const socketio = require('socket.io')
 
 const Filter = require('bad-words')
 
+const { generateMessage } = require('./utils/messages')
+
 const app = express()
 
 const server = http.createServer(app)
@@ -21,8 +23,11 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
   console.log('New WebSocket connection')
 
-  socket.emit('message', 'Welcome!')
-  socket.broadcast.emit('message', 'A new user has joined!')
+  socket.emit('message', generateMessage('Welcome!'))
+  socket.broadcast.emit(
+    'message',
+    generateMessage('A new user has joined!')
+  )
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter()
@@ -31,7 +36,7 @@ io.on('connection', (socket) => {
       return callback('profane is not allowed!')
     }
 
-    io.emit('message', message)
+    io.emit('message', generateMessage(message))
 
     callback()
   })
@@ -43,7 +48,7 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left!')
+    io.emit('message', generateMessage('A user has left!'))
   })
 })
 
