ba92a50fa30433f3e1e857463331bdafaae52de3
Jason Stathopulos <jstathopulos@gmail.com>
2019-08-19 21:19:53 -0400

    Add Func Export/Import Example For 1st Challenge
    Add function export/import example for 1st Challenge.

--- intro-graphql-course/graphql-basics/src/index.js
+++ intro-graphql-course/graphql-basics/src/index.js
@@ -1,4 +1,8 @@
 import location, { message, name, getGreeting } from './myModule'
+import add, { subtract } from './math'
+
+console.log(add(2, 3))
+console.log(subtract(6, 5))
 
 console.log(message)
 console.log(name)
