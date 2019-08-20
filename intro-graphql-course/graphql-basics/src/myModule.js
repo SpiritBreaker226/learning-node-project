77bc2ae0b36ded811fb7ef06a41fc2b9f1c8ce55
Jason Stathopulos <jstathopulos@gmail.com>
2019-08-20 19:58:42 -0400

    Remove Export/Import Example Code No Longer Needed

--- intro-graphql-course/graphql-basics/src/myModule.js
+++
@@ -1,9 +0,0 @@
-const message = 'Some message from myModule.js'
-
-const name = 'Jason'
-
-const location = 'Ottawa'
-
-const getGreeting = (name) => `Welcome to the course ${name}`
-
-export { message, name, getGreeting, location as default }
