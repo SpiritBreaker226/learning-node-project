25e33da4703db31da8d658886f68ea194577a598
Jason Stathopulos <jstathopulos@gmail.com>
2019-08-19 21:01:23 -0400

    Add Export / Import Example

--- intro-graphql-course/graphql-basics/src/index.js
+++ intro-graphql-course/graphql-basics/src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello GraphQL')
+import location, { message, name, getGreeting } from './myModule'
+
+console.log(message)
+console.log(name)
+console.log(`default export ${location}`)
+console.log(getGreeting('Lisa'))
