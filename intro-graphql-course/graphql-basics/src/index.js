cab908bb6a63940076154e4b998f31bcfbe948af
Jason Stathopulos <jstathopulos@gmail.com>
2019-08-20 20:58:33 -0400

    Add All Scalar Types Example
    Add all of the scalar types as an example which are String, Boolean,
    Int, Float, ID.

--- intro-graphql-course/graphql-basics/src/index.js
+++ intro-graphql-course/graphql-basics/src/index.js
@@ -1,29 +1,35 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// Scalar Types - String, Boolean, Int, Float, ID
+
 const typeDefs = `
   type Query {
-    hello: String!
+    id: ID!
     name: String!
-    location: String!
-    bio: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
   }
 `
 
 const resolvers = {
   Query: {
-    hello() {
-      return 'This ia my first query!'
+    id() {
+      return 'abc123' // ID type can be a number but is convert to a stirng
     },
     name() {
       return 'Jason'
     },
-    location() {
-      return 'Montreal'
+    age() {
+      return 29
     },
-    bio() {
-      return 'There is no bio'
+    employed() {
+      return false
     },
-  }
+    gpa() {
+      return null // to show want a non-bang is allow to do have null values
+    },
+  },
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
