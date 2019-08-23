5ffb6631208350c62fd56ba41549a97bb96d4568
Jason Stathopulos <jstathopulos@gmail.com>
2019-08-22 20:26:00 -0400

    Remove Examples of Scalar Types No Longer Needed

--- intro-graphql-course/graphql-basics/src/index.js
+++ intro-graphql-course/graphql-basics/src/index.js
@@ -1,54 +1,12 @@
 import { GraphQLServer } from 'graphql-yoga'
 
-// Scalar Types - String, Boolean, Int, Float, ID
-
 const typeDefs = `
   type Query {
-    id: ID!
-    name: String!
-    age: Int!
-    employed: Boolean!
-    gpa: Float
-    title: String!
-    price: Float!
-    releaseYear: Int
-    rating: Float
-    inStock: Boolean!
   }
 `
 
 const resolvers = {
   Query: {
-    id() {
-      return 'abc123' // ID type can be a number but is convert to a stirng
-    },
-    name() {
-      return 'Jason'
-    },
-    age() {
-      return 29
-    },
-    employed() {
-      return false
-    },
-    gpa() {
-      return null // to show want a non-bang is allow to do have null values
-    },
-    title() {
-      return 'My Book'
-    },
-    price() {
-      return 1.23
-    },
-    releaseYear() {
-      return null
-    },
-    rating() {
-      return 4.5
-    },
-    inStock() {
-      return false
-    },
   },
 }
 
